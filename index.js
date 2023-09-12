5727e68910d660a7647a463839e0d96921ad33da
mephaline <suaramos88@gmail.com>
2023-09-12 16:53:35 -0500

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names) {
+    const thankYouMessages = [];
+  
+    for (const name of names) {
+      thankYouMessages.push(`Thank you, ${name}, for the wonderful surprise gift!`);
+    }
+    
+    return thankYouMessages;
+  }
+  function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
